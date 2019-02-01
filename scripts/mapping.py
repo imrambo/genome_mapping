87a3fc1cb39d74d7896211fe009c0a7262d19c07
Ian Rambo <ian@dhcp-146-6-184-232.utmsi.utexas.edu>
2019-01-31 18:04:27 -0600

    vars for argument parser

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -36,12 +36,11 @@ help = 'memory per thread for samtools sort. Specify an integer with K, M, or G 
 parser.add_argument('--nslice', dest = 'nslice', type = int, nargs = 1, action = 'store',
 help = 'number of fastq headers to slice for determining if interleaved.')
 
-args = parser.parse_args()
-print(args)
+args = vars(parser.parse_args())
 #=============================================================================
 #Options for bwa mem, samtools sort, depthfile
-bwa_mem_opts = {'-t':args.bwa_thread}
-samtools_sort_opts = {'-@':args.samsort_thread, '-m':args.samsort_mem}
+bwa_mem_opts = {'-t':args['bwa_thread']}
+samtools_sort_opts = {'-@':args['samsort_thread'], '-m':args['samsort_mem']}
 #Options for depth file
 depthfile_opts = {'--noIntraDepthVariance':''}
 #------------------------------------------------------------------------------
@@ -54,38 +53,38 @@ depthfile_optstring = optstring_join(depthfile_opts)
 ###
 #=============================================================================
 #Create output directory if it does not exist
-if not os.path.exists(args.outdir):
-    os.makedirs(args.outdir)
+if not os.path.exists(args['outdir']):
+    os.makedirs(args['outdir'])
 else:
     pass
 #=============================================================================
 #Index the genome file
-bwa_index(args.genome)
+bwa_index(args['genome'])
 #------------------------------------------------------------------------------
 #Generate list of input FASTQ files using sample IDs
-fastq_list = source_list_generator(args.sids, args.fastq_dir, '.fastq.gz')
+fastq_list = source_list_generator(args['sids'], args['fastq_dir'], '.fastq.gz')
 #------------------------------------------------------------------------------
 mapping_targets = list()
-fastq_dict = find_fastq_pairs(fastq_list, nslice = args.nslice*4)
+fastq_dict = find_fastq_pairs(fastq_list, nslice = args['nslice']*4)
 
 for key in fastq_dict:
-    maptarg = [os.path.join(args.outdir, os.path.splitext(os.path.basename(fastq_dict[key]['R1']))[0] + x) for x in ['.reduced.sam', '.reduced.bam']]
+    maptarg = [os.path.join(args['outdir'], os.path.splitext(os.path.basename(fastq_dict[key]['R1']))[0] + x) for x in ['.reduced.sam', '.reduced.bam']]
     if fastq_dict[key]['R2'] == 'interleaved':
-        bwa_sam_intl(targets = maptarg, sources = [args.genome, fastq_dict[key]['R1']], bopts = bwa_optstring, sopts = samtools_sort_optstring)
+        bwa_sam_intl(targets = maptarg, sources = [args['genome'], fastq_dict[key]['R1']], bopts = bwa_optstring, sopts = samtools_sort_optstring)
         mapping_targets.extend(maptarg)
 
     else:
-        bwa_sam_r1r2(targets = maptarg, sources = [args.genome, fastq_dict[key]['R1'], fastq_dict[key]['R2']], bopts = bwa_optstring, sopts = samtools_sort_optstring)
+        bwa_sam_r1r2(targets = maptarg, sources = [args['genome'], fastq_dict[key]['R1'], fastq_dict[key]['R2']], bopts = bwa_optstring, sopts = samtools_sort_optstring)
         mapping_targets.extend(maptarg)
 #------------------------------------------------------------------------------
 #Depth file
-depthfile_target = os.path.join(args.outdir, os.path.splitext(os.path.basename(args.genome))[0] + '_cov')
+depthfile_target = os.path.join(args['outdir'], os.path.splitext(os.path.basename(args['genome']))[0] + '_cov')
 depthfile_sources = [m for m in mapping_targets if re.match(r'.*?\.reduced\.bam', m)]
 
 depth_file(target = depthfile_target, sources = depthfile_sources, opts = depthfile_optstring)
 #------------------------------------------------------------------------------
 #Network file
-network_source = [m for m in mapping_targets if args.netsam in m and m.endswith('.sam')][0]
-network_target = os.path.join(args.outdir, args.netsam + '.txt')
+network_source = [m for m in mapping_targets if args['netsam'] in m and m.endswith('.sam')][0]
+network_target = os.path.join(args['outdir'], args['netsam'] + '.txt')
 
 network_file(target = network_target, source = network_source)
