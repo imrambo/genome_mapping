894faa1a4761840d58e2df24847dc241acfd8933
Ian Rambo <ian@dhcp-146-6-184-232.utmsi.utexas.edu>
2019-02-09 19:19:12 -0600

    fastq warning message

--- src/fastq_pair.py
+++ src/fastq_pair.py
@@ -61,7 +61,7 @@ def find_fastq_pairs(fastq_list, nslice = 800):
         #if not fastq_dict[key]['R2'] or not fastq_dict[key]['R1']:
         if not 'R2' in fastq_dict or not 'R1' in fastq_dict:
             solo = [fastq_dict[key][v] for v in fastq_dict[key].keys()][0]
-            warning_message = "No pair for non-interleaved reads %s found" % solo
+            warning_message = "No pair for reads %s found, please include read pair or check if the sequences are correctly interleaved" % solo
             warn(warning_message)
 
     return fastq_dict
