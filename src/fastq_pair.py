983f362509a5f5af3ba965e83f4f61f0f0071cc7
Ian Rambo <ian@dhcp-146-6-184-232.utmsi.utexas.edu>
2019-02-11 16:05:08 -0600

    source_list_generator: change iglob to glob for compatability

--- src/fastq_pair.py
+++ src/fastq_pair.py
@@ -89,9 +89,11 @@ def source_list_generator(id_string, source_dir, extension):
         id_list = id_string.split(',')
         for i in id_list:
             idGlob = os.path.join(os.path.abspath(source_dir), '%s*%s' % (i, extension))
-            source_list.extend(list(glob.iglob(idGlob, recursive = False)))
+            #source_list.extend(list(glob.iglob(idGlob, recursive = False)))
+            source_list.extend(list(glob.glob(idGlob, recursive = False)))
     else:
         idGlob = os.path.join(os.path.abspath(source_dir), '%s*%s' % (id_string, extension))
-        source_list.extend(list(glob.iglob(idGlob, recursive = False)))
+        #source_list.extend(list(glob.iglob(idGlob, recursive = False)))
+        source_list.extend(list(glob.glob(idGlob, recursive = False)))
 
     return source_list
