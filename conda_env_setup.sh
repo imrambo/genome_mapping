3144854675ca39d5fb608f1477720df62aa12a70
Ian Rambo <ian@Ians-MacBook-Pro.local>
2020-02-01 12:23:34 -0800

    remove conda_config.sh

--- conda_env_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-envName=$1
-conda create -n $envName python=3.7.4 && \
-conda install -c conda-forge -n $envName python-magic scons && \
-conda install -c bioconda -n $envName samtools=1.10 bwa
