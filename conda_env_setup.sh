ed00b2440db90efb22840d86263f763aeee2e3f1
Ian Rambo <ian@Ians-MacBook-Pro.local>
2020-02-01 02:34:39 -0800

    markdup builders

--- conda_env_setup.sh
+++ conda_env_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 envName=$1
-conda create -n $envName python=3.7 && \
+conda create -n $envName python=3.7.4 && \
 conda install -c conda-forge -n $envName python-magic && \
 conda install -c conda-forge -n $envName scons && \
 conda install -c bioconda -n $envName samtools=1.10 bwa
